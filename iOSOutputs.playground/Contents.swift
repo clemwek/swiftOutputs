f814f5d72b1c54ef7632409e2aa9fc4e2d07ff13
dhywty <clemwek@gmail.com>
2017-11-14 12:07:48 +0300

    output one

--- iOSOutputs.playground/Contents.swift
+++ iOSOutputs.playground/Contents.swift
@@ -1,1 +1,15 @@
+//: ### Output   1:
+//: a program that iterates over an arbitrary range of integers and prints descriptive phrases for each.
 
+
+func testFunc(limit: Int) {
+    for i in 0...limit {
+        let isEven: Bool = i % 2 == 0 ? true : false
+        switch isEven {
+        case true:
+            print("\(i) is an even Number")
+        default:
+            print("\(i) is an odd number")
+        }
+    }
+}
