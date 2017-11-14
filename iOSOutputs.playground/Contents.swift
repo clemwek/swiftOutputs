a9bff843d0013ae7e11806e397d3435e5a9df902
dhywty <clemwek@gmail.com>
2017-11-14 12:09:41 +0300

    output 2

--- iOSOutputs.playground/Contents.swift
+++ iOSOutputs.playground/Contents.swift
@@ -13,3 +13,47 @@ func testFunc(limit: Int) {
         }
     }
 }
+
+testFunc(limit: 23)
+
+
+//: ### Output 2:
+//: Using   a   Swift   playground,   write   a   program   that   combines   an   array   of   dictionaries   into   a   single dictionary   such   that   the   value   associated   with   each   key   is   a   _set_   containing   the   unique   values associated   with   that   key   in   the   input   dictionaries.   The   program   should   then   print   out   the _sorted_   results   in   human-readable   form.
+
+let myArray = [
+    ["Girls": ["Tiffany", "Kim", "Tiffany"]],
+    ["Girls": ["jane", "tess"]],
+    ["Boys": ["Martin"]]
+]
+
+
+var myDict: [String : [String]] = [:]
+
+let reducedDictionary = myArray
+    .flatMap(){$0}
+    .reduce([String: Set<String>]()){ (result, value) in
+        if (result.keys).contains(value.key){
+            
+            let tempValue = result[value.key]
+            //container for our temporary dict
+            var tempDictionary = result
+            
+            // pick the dictionary value
+            let dictValue = value.value
+            
+            //convert the dict value to a set
+            let newSet = Set(dictValue)
+            
+            let newValue = tempValue?.union(newSet)
+            tempDictionary.updateValue(newValue ?? Set(), forKey: value.key)
+            return tempDictionary
+        }
+        else{
+            var tempDictionary = result
+            tempDictionary[value.key] = Set(value.value)
+            return tempDictionary
+        }
+}
+
+print(reducedDictionary)
+
